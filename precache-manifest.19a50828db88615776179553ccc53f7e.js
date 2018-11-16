ae365ec51b73eaf538c5ae6469580fbdc2ae9474
JOLee83 <lee.justin.oliver@gmail.com>
2018-11-15 23:07:29 -0500

    Updates

---
+++ precache-manifest.19a50828db88615776179553ccc53f7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae9361cc6f9133065928",
+    "url": "/static/js/main.ae9361cc.chunk.js"
+  },
+  {
+    "revision": "5b1be3dcca61194d06f3",
+    "url": "/static/js/1.5b1be3dc.chunk.js"
+  },
+  {
+    "revision": "ae9361cc6f9133065928",
+    "url": "/static/css/main.d1c15736.chunk.css"
+  },
+  {
+    "revision": "8337f0ef6d21f4df830e3fd73b7f900c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/justin-oliver.com/precache-manifest.f68fab7735e484bdd17292b1947a33d6.js"
+  "/precache-manifest.19a50828db88615776179553ccc53f7e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/justin-oliver.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
