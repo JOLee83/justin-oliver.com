549a20c4c1aafc498c57d52f2ff6a5044e9b97db
JOLee83 <lee.justin.oliver@gmail.com>
2018-12-18 15:14:58 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9560c984e8413ac100bb602403c01c8.js"
+  "/precache-manifest.7670b044631ff8dd072261122938ce8a.js"
 );
 
 workbox.clientsClaim();
