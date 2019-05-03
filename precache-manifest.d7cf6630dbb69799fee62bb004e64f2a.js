0140e0cd94ab7fe823cc78ab4e5409a50e63e421
JOLee83 <lee.justin.oliver@gmail.com>
2019-05-02 22:21:48 -0700

    Updates

--- precache-manifest.d7cf6630dbb69799fee62bb004e64f2a.js
+++ precache-manifest.d7cf6630dbb69799fee62bb004e64f2a.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/static/media/fa-brands-400.8b7a9afd.woff"
   },
   {
-    "revision": "a2f79ce3f75ae35042ff",
+    "revision": "0456176bfdcab8dfa200",
     "url": "/static/css/main.54ef1ade.chunk.css"
   },
   {
@@ -28,20 +28,20 @@ self.__precacheManifest = [
     "url": "/static/media/fa-regular-400.0b5e3a54.woff"
   },
   {
-    "revision": "b48c48ea8457846a5695b139c377d3d1",
-    "url": "/static/media/fa-regular-400.b48c48ea.ttf"
-  },
-  {
     "revision": "6493321d567eb0f22bd5112fbcf044a8",
     "url": "/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/static/media/fa-regular-400.b48c48ea.ttf"
   },
   {
     "revision": "659c4d58b00226541ef95c3a76e169c5",
     "url": "/static/media/fa-brands-400.659c4d58.woff2"
   },
   {
-    "revision": "a2f79ce3f75ae35042ff",
-    "url": "/static/js/main.a2f79ce3.chunk.js"
+    "revision": "0456176bfdcab8dfa200",
+    "url": "/static/js/main.0456176b.chunk.js"
   },
   {
     "revision": "fb493903265cad425ccdf8e04fc2de61",
@@ -80,7 +80,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.21a03d81.chunk.css"
   },
   {
-    "revision": "895f7d1879e4097a4a05fab1142dcee7",
+    "revision": "111f74f7ab12a0f8078e46bb124ee045",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec9cf559759b6cdf53533d1a280290a8.js"
+  "/precache-manifest.d7cf6630dbb69799fee62bb004e64f2a.js"
 );
 
 workbox.clientsClaim();
