b11eeca92986ea2e5b9e9da33f07f1f1d1237c37
Justin Oliver Lee <JOLee83@JOLee83-MacBook-Air.local>
2019-10-23 13:36:19 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c5a4b40776724ff83ebb4c981d5796d.js"
+  "/precache-manifest.a3eb9655cd13aaf2c48648815bfabe45.js"
 );
 
 workbox.clientsClaim();
