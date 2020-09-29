7afbe08f7156d1efa82554796b93fbb2f1a4d870
JOLee83 <JOLee83@JOLee83-MacBook-Air.local>
2020-09-29 16:45:51 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5442a12a2abd5174df8c21e405930e91.js"
+  "/precache-manifest.7817cf574d5bd1cf45e6492158d41e6e.js"
 );
 
 workbox.clientsClaim();
